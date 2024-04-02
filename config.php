88cf0ccf28069236ce794eab60e5bf81f0f1982f
matteohcf <matteo.carrara.teo@gmail.com>
2024-04-02 13:43:25 +0200

    Push new config 02/04 13:43

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Configurazione del database per la sezione preventivi (per non far vedere i dati del db a tutti)
 $db_host = "localhost";
-$db_user = "root";
-$db_password = "root";
+$db_user = "matteohcf";
+$db_password = "matteo2004";
 $db_name = "design_palette";
 ?>
